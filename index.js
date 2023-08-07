57660de3828a6092aec65950b358b59ef0e58f69
K Rajesh <131753283+Rajeshkumarachari@users.noreply.github.com>
2023-08-07 19:16:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function changeColor() {
+  document.getElementById("oned").style.borderColor = "green";
+  document.getElementById("oned").style.backgroundColor = "lightgreen";
+  document.getElementById("oned1").style.backgroundColor = "lightgreen";
+  document.getElementById("oned2").style.backgroundColor = "lightgreen";
+  document.getElementById("oned3").style.backgroundColor = "lightgreen";
+  document.getElementById("oned4").style.backgroundColor = "lightgreen";
+  document.getElementById("oned5").style.backgroundColor = "lightgreen";
+}
+
+function changeColor1() {
+  document.getElementById("two").style.borderColor = "green";
+}
+function changeColor2() {
+  document.getElementById("three").style.borderColor = "green";
+  document.getElementById("three").style.backgroundColor = "lightgreen";
+  document.getElementById("three1").style.backgroundColor = "lightgreen";
+  document.getElementById("three2").style.backgroundColor = "lightgreen";
+  document.getElementById("three3").style.backgroundColor = "lightgreen";
+  document.getElementById("three4").style.backgroundColor = "lightgreen";
+  document.getElementById("three5").style.backgroundColor = "lightgreen";
+}
